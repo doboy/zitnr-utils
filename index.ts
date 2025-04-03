9178622d157a091a075822640bad4dd666dfe954
Huan (Zack) Do <doboy0@gmail.com>
2025-04-03 18:52:43 +0700

    add tests

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-export * from "./courts";
 export * from "./types";
 export * from "./parks";
 export * from "./combineTimes";
--- parks.ts
+++ parks.ts
@@ -1,15 +1,15 @@
-import { Court, CourtWithParkId, Park } from "./types";
+import { CourtWithParkId, Park } from "./types";
 
 export const MillerPark: Park = {
   id: 1373,
   name: "Miller Playfield",
   courts: [{
     id: 1373,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 1374,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "07:00:00",
@@ -25,11 +25,11 @@ export const MountBakerPark: Park = {
   name: "Mount Baker Park",
   courts: [{
     id: 1377,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 1378,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "06:00:00",
@@ -45,11 +45,11 @@ export const BeaconHillPlayfield: Park = {
   name: "Beacon Hill Playfield",
   courts: [{
     id: 1319,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1320,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: false,
   }],
   startTime: "07:00:00",
@@ -65,16 +65,20 @@ export const BitterLakePlayfield: Park = {
   name: "Bitter Lake Playfield",
   courts: [{
     id: 1315,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1316,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1317,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
+  }, {
+    id: 1318,
+    courtNo: "4",
+    canReserve: false,
   }],
   startTime: "07:00:00",
   endTime: "21:45:00",
@@ -89,16 +93,20 @@ export const RainierBeachPlayfield: Park = {
   name: "Rainier Beach Playfield",
   courts: [{
     id: 1379,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1380,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1381,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
+  }, {
+    id: 1382,
+    courtNo: "4",
+    canReserve: false,
   }],
   startTime: "08:30:00",
   endTime: "23:00:00",
@@ -113,11 +121,11 @@ export const MagnoliaPark: Park = {
   name: "Magnolia Park",
   courts: [{
     id: 1361,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 1362,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "04:00:00",
@@ -133,11 +141,11 @@ export const GreenLakeParkWest: Park = {
   name: "Green Lake Park West",
   courts: [{
     id: 181,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 183,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: false,
   }],
   startTime: "00:00:00",
@@ -153,15 +161,15 @@ export const GreenLakeParkEast: Park = {
   name: "Green Lake Park East",
   courts: [{
     id: 2906,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 2907,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 2908,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
   }],
   startTime: "07:00:00",
@@ -172,21 +180,76 @@ export const GreenLakeParkEast: Park = {
   },
 };
 
-export const LowerWoodlandPlayfieldUpper: Park = {
-  id: 369,
-  name: "Lower Woodland Playfield Upper",
-  courts: [{
+export const LowerWoodlandPlayfield: Park = {
+  id: 353,
+  name: "Lower Woodland Playfield",
+  courts: [
+  {
+    id: 353,
+    courtNo: "1",
+    canReserve: false
+  },
+  {
+    id: 354,
+    courtNo: "2",
+    canReserve: false
+  },
+  {
+    id: 355,
+    courtNo: "3",
+    canReserve: true,
+  },
+  {
+    id: 356,
+    courtNo: "4",
+    canReserve: true,
+  },
+  {
+    id: 357,
+    courtNo: "5",
+    canReserve: true,
+  },
+  {
+    id: 358,
+    courtNo: "6",
+    canReserve: true,
+  },
+  {
+    id: 359,
+    courtNo: "7",
+    canReserve: true,
+  },
+  {
+    id: 360,
+    courtNo: "8",
+    canReserve: true,
+  },
+  {
+    id: 361,
+    courtNo: "9",
+    canReserve: false
+  },
+  {
+    id: 362,
+    courtNo: "10",
+    canReserve: false
+  },
+  {
     id: 369,
-    courtNo: "Court 1",
+    courtNo: "1 - Upper",
     canReserve: true,
   }, {
     id: 370,
-    courtNo: "Court 2",
+    courtNo: "2 - Upper",
     canReserve: true,
   }, {
     id: 371,
-    courtNo: "Court 3",
+    courtNo: "3 - Upper",
     canReserve: true,
+  }, {
+    id: 372,
+    courtNo: "4 - Upper",
+    canReserve: false,
   }],
   startTime: "07:00:00",
   endTime: "21:00:00",
@@ -201,27 +264,27 @@ export const IngrahamHS: Park = {
   name: "Ingraham HS",
   courts: [{
     id: 1095,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1096,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1097,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
   }, {
     id: 1098,
-    courtNo: "Court 4",
+    courtNo: "4",
     canReserve: true,
   }, {
     id: 2695,
-    courtNo: "Court 5",
+    courtNo: "5",
     canReserve: true,
   }, {
     id: 2696,
-    courtNo: "Court 6",
+    courtNo: "6",
     canReserve: true,
   }],
   startTime: "08:30:00",
@@ -237,11 +300,11 @@ export const GarfieldPlayfield: Park = {
   name: "Garfield Playfield",
   courts: [{
     id: 1341,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 1342,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "06:00:00",
@@ -253,19 +316,19 @@ export const VolunteerPark: Park = {
   name: "Volunteer Park",
   courts: [{
     id: 363,
-    courtNo: "Court 3 - Lower",
+    courtNo: "3 - Lower",
     canReserve: true,
   }, {
     id: 364,
-    courtNo: "Court 4 - Lower",
+    courtNo: "4 - Lower",
     canReserve: true,
   }, {
     id: 365,
-    courtNo: "Court 1 - Upper",
+    courtNo: "1 - Upper",
     canReserve: true,
   }, {
     id: 366,
-    courtNo: "Court 2 - Upper",
+    courtNo: "2 - Upper",
     canReserve: false,
   }],
   startTime: "06:00:00",
@@ -277,11 +340,11 @@ export const MadronaPlayground: Park = {
   name: "Madrona Playground",
   courts: [{
     id: 2279,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 2280,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "06:00:00",
@@ -293,11 +356,11 @@ export const SamSmithPark: Park = {
   name: "Sam Smith (I90 Lid) Park",
   courts: [{
     id: 1391,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1392,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: false,
   }],
   startTime: "04:00:00",
@@ -309,11 +372,11 @@ export const MontlakePlayfield: Park = {
   name: "Montlake Playfield",
   courts: [{
     id: 1375,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 1376,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "04:00:00",
@@ -325,11 +388,11 @@ export const RogersPlayfield: Park = {
   name: "Rogers Playfield",
   courts: [{
     id: 1389,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1390,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: false,
   }],
   startTime: "08:30:00",
@@ -341,27 +404,27 @@ export const AYTCOutdoor: Park = {
   name: "AYTC Outdoor",
   courts: [{
     id: 279,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 280,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 281,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
   }, {
     id: 282,
-    courtNo: "Court 4",
+    courtNo: "4",
     canReserve: true,
   }, {
     id: 283,
-    courtNo: "Court 5",
+    courtNo: "5",
     canReserve: true,
   }, {
     id: 284,
-    courtNo: "Court 6",
+    courtNo: "6",
     canReserve: true,
   }],
   startTime: "07:00:00",
@@ -373,11 +436,11 @@ export const MadisonPark: Park = {
   name: "Madison Park",
   courts: [{
     id: 1359,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1360,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: false,
   }],
   startTime: "04:00:00",
@@ -389,11 +452,11 @@ export const ObservatoryPark: Park = {
   name: "Observatory Park",
   courts: [{
     id: 1119,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: false,
   }, {
     id: 1120,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }],
   startTime: "08:30:00",
@@ -405,15 +468,15 @@ export const DavidRodgersPark: Park = {
   name: "David Rodgers Park",
   courts: [{
     id: 1330,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1331,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1332,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: false,
   }],
   startTime: "04:00:00",
@@ -425,19 +488,19 @@ export const JeffersonParkLid: Park = {
   name: "Jefferson Park Lid",
   courts: [{
     id: 777,
-    courtNo: "Court Upper 1",
+    courtNo: "Upper 1",
     canReserve: true,
   }, {
     id: 778,
-    courtNo: "Court Upper 2",
+    courtNo: "Upper 2",
     canReserve: false,
   }, {
     id: 2277,
-    courtNo: "Court Lower 1",
+    courtNo: "Lower 1",
     canReserve: true,
   }, {
     id: 2278,
-    courtNo: "Court Lower 2",
+    courtNo: "Lower 2",
     canReserve: true,
   }],
   startTime: "07:00:00",
@@ -449,11 +512,11 @@ export const WallingfordPlayfield: Park = {
   name: "Wallingford Playfield",
   courts: [{
     id: 1408,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1409,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: false,
   }],
   startTime: "08:30:00",
@@ -465,19 +528,19 @@ export const LaurelhurstPlayfield: Park = {
   name: "Laurelhurst Playfield",
   courts: [{
     id: 1353,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1354,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1355,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
   }, {
     id: 1356,
-    courtNo: "Court 4",
+    courtNo: "4",
     canReserve: false,
   }],
   startTime: "07:00:00",
@@ -489,19 +552,19 @@ export const RainierPlayfield: Park = {
   name: "Rainier Playfield",
   courts: [{
     id: 1383,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1384,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1385,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
   }, {
     id: 1386,
-    courtNo: "Court 4",
+    courtNo: "4",
     canReserve: false,
   }],
   startTime: "07:00:00",
@@ -513,31 +576,355 @@ export const SolsticePark: Park = {
   name: "Solstice Park",
   courts: [{
     id: 1393,
-    courtNo: "Court 1",
+    courtNo: "1",
     canReserve: true,
   }, {
     id: 1394,
-    courtNo: "Court 2",
+    courtNo: "2",
     canReserve: true,
   }, {
     id: 1395,
-    courtNo: "Court 3",
+    courtNo: "3",
     canReserve: true,
   }, {
     id: 1396,
-    courtNo: "Court 4",
+    courtNo: "4",
     canReserve: false,
   }, {
     id: 1397,
-    courtNo: "Court 5",
+    courtNo: "5",
     canReserve: false,
   }, {
     id: 1398,
-    courtNo: "Court 6",
+    courtNo: "6",
     canReserve: false,
   }],
   startTime: "07:00:00",
   endTime: "22:00:00",
+};
+
+export const HiawathaPlayfield : Park = {
+  id: 1346,
+  name: "Hiawatha Playfield",
+  courts: [{
+    id: 1346,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1347,
+    courtNo: "2",
+    canReserve: true,
+  }, {
+    id: 1348,
+    courtNo: "3",
+    canReserve: false,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00",
+}
+
+export const DelridgePlayfield: Park = {
+  id: 1335,
+  name: "Delridge Playfield",
+  courts: [{
+    id: 1335,
+    courtNo: "1",
+    canReserve: false,
+  }, {
+    id: 1336,
+    courtNo: "2",
+    canReserve: true,
+  }],
+  startTime: "08:00:00",
+  endTime: "23:00:00",
+};
+
+export const DearbornPark : Park = {
+  id: 1333,
+  name: "Dearborn Park",
+  courts: [{
+    id: 1333,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1334,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "04:00:00",
+  endTime: "23:30:00",
+};
+
+export const BryantPlayground : Park = {
+  id: 1325,
+  name: "Bryant Playground",
+  courts: [{
+    id: 1325,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1326,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "07:00:00",
+  endTime: "21:00:00",
+};
+
+export const AlkiPlayfield: Park = {
+  id: 1146,
+  name: "Alki Playfield",
+  courts: [{
+    id: 1146,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1147,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00"
+};
+
+export const BallardHS: Park = {
+  id: 1089,
+  name: "Ballard HS",
+  courts: [{
+    id: 1089,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1091,
+    courtNo: "2",
+    canReserve: true,
+  }, {
+    id: 1092,
+    courtNo: "3",
+    canReserve: true,
+  }, {
+    id: 1093,
+    courtNo: "4",
+    canReserve: true,
+  }],
+  startTime: "08:30:00",
+  endTime: "21:00:00",
+};
+
+export const SewardPark : Park = {
+  id: 373,
+  name: "Seward Park",
+  courts: [{
+    id: 373,
+    courtNo: "1",
+    canReserve: false,
+  }, {
+    id: 2437,
+    courtNo: "2",
+    canReserve: true,
+  }],
+  startTime: "06:00:00",
+  endTime: "22:00:00",
+}
+
+export const RiverviewPlayfield : Park = {
+  id: 1387,
+  name: "Riverview Playfield",
+  courts: [{
+    id: 1387,
+    courtNo: "1",
+    canReserve: false,
+  }, {
+    id: 1388,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00",
+};
+
+export const DiscoveryPark : Park = {
+  id: 1337,
+  name: "Discovery Park",
+  courts: [{
+    id: 1337,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1338,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "04:00:00",
+  endTime: "23:30:00",
+};
+
+export const SealthHSComplex : Park = {
+  id: 2281,
+  name: "Sealth HS Complex",
+  courts: [{
+    id: 2281,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 2282,
+    courtNo: "2",
+    canReserve: true,
+  }, {
+    id: 2283,
+    courtNo: "3",
+    canReserve: true,
+  }, {
+    id: 2284,
+    courtNo: "4",
+    canReserve: true,
+  }, {
+    id: 2285,
+    courtNo: "5",
+    canReserve: true,
+  }, {
+    id: 2286,
+    courtNo: "6",
+    canReserve: true,
+  }],
+  startTime: "08:30:00",
+  endTime: "21:00:00",
+};
+
+export const MeadowbrookPlayfield : Park = {
+  id: 1367,
+  name: "Meadowbrook Playfield",
+  courts: [{
+    id: 1367,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1368,
+    courtNo: "2",
+    canReserve: true,
+  }, {
+    id: 1369,
+    courtNo: "3",
+    canReserve: true,
+  }, {
+    id: 1370,
+    courtNo: "4",
+    canReserve: false,
+  }, {
+    id: 1371,
+    courtNo: "5",
+    canReserve: false,
+  }, {
+    id: 1372,
+    courtNo: "6",
+    canReserve: false,
+  }],
+  startTime: "07:00:00",
+  endTime: "22:00:00",
+};
+
+export const WaltHundleyPlayfield : Park = {
+  id: 1410,
+  name: "Walt Hundley Playfield",
+  courts: [{
+    id: 1410,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1411,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00",
+};
+
+export const SoundviewPlayfield : Park = {
+  id: 1399,
+  name: "Soundview Playfield",
+  courts: [{
+    id: 1399,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1400,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00",
+};
+
+export const MagnoliaPlayfield : Park = {
+  id: 1363,
+  name: "Magnolia Playfield",
+  courts: [{
+    id: 1363,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1364,
+    courtNo: "2",
+    canReserve: false,
+  }, {
+    id: 1365,
+    courtNo: "3",
+    canReserve: true,
+  }, {
+    id: 1366,
+    courtNo: "4",
+    canReserve: true,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00",
+};    
+
+export const GilmanPlayfield : Park = {
+  id: 1344,
+  name: "Gilman Playfield",
+  courts: [{
+    id: 1344,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1345,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "08:30:00",
+  endTime: "23:00:00",
+};
+
+export const FroulaPlayground : Park = {
+  id: 1339,
+  name: "Froula Playground",
+  courts: [{
+    id: 1339,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1340,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "07:00:00",
+  endTime: "21:00:00",
+};
+
+export const BrightonPlayfield : Park = {
+  id: 1323,
+  name: "Brighton Playfield",
+  courts: [{
+    id: 1323,
+    courtNo: "1",
+    canReserve: true,
+  }, {
+    id: 1324,
+    courtNo: "2",
+    canReserve: false,
+  }],
+  startTime: "06:00:00",
+  endTime: "23:00:00",
 };
 
 export const PARKS: Park[] = [
@@ -550,7 +937,7 @@ export const PARKS: Park[] = [
   MagnoliaPark,
   GreenLakeParkWest,
   GreenLakeParkEast,
-  LowerWoodlandPlayfieldUpper,
+  LowerWoodlandPlayfield,
   IngrahamHS,
 
   // Other Seattle parks
@@ -568,6 +955,24 @@ export const PARKS: Park[] = [
   WallingfordPlayfield,
   LaurelhurstPlayfield,
   RainierPlayfield,
+  HiawathaPlayfield,
+  SolsticePark,
+  DelridgePlayfield,
+  DearbornPark,
+  BryantPlayground,
+  AlkiPlayfield,
+  BallardHS,
+  SewardPark,
+  RiverviewPlayfield,
+  DiscoveryPark,
+  SealthHSComplex,
+  MeadowbrookPlayfield,
+  WaltHundleyPlayfield,
+  SoundviewPlayfield,
+  MagnoliaPlayfield,
+  GilmanPlayfield,
+  FroulaPlayground,
+  BrightonPlayfield,
 ];
 
 export const parksById: Record<number, Park> = PARKS.reduce((acc, park) => {
